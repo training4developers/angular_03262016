499260a4b9e98e371ea63df4fe2c1ba66d62d1c7
Eric W. Greene <eric@ericwgreene.com>
2016-03-26 14:59:44 -0700

    added create code

--- start/www/js/site.js
+++ start/www/js/site.js
@@ -12,7 +12,14 @@
 				.state("home", {
 					url: "/",
 					controller: "HomeCtrl",
-					templateUrl: "/tpls/widgets.tpl"
+					templateUrl: "/tpls/widgets.tpl",
+					resolve: {
+						widgetsData: function(widgets) {
+							return widgets.getAll().then(function(results) {
+								return results.data;
+							});
+						}
+					}
 				})
 				.state("create", {
 					url: "/widgets/create",
@@ -22,7 +29,15 @@
 				.state("view", {
 					url: "/widgets/:widgetId",
 					controller: "ViewCtrl",
-					templateUrl: "/tpls/view-widget.tpl"
+					templateUrl: "/tpls/view-widget.tpl",
+					resolve: {
+						widgetData: function(widgets, $stateParams) {
+							return widgets.get($stateParams.widgetId)
+								.then(function(results) {
+									return results.data;
+								});
+						}
+					}
 				})
 				.state("edit", {
 					url: "/widgets/:widgetId/edit",
@@ -49,33 +64,14 @@
 		// 		});
 		//
 		// })
-		.factory("widgets", function() {
-
-			var widgets = [
-				{ "id": 1, "name": "Small Red Widget",   "description": "A small, red widget.",   "color": "red",    "size": "small",  "quantity": 2 },
-				{ "id": 2, "name": "Medium Blue Widget", "description": "A medium, blue widget.", "color": "blue",   "size": "medium", "quantity": 15 },
-				{ "id": 3, "name": "Large Green Widget", "description": "A large, green widget.", "color": "green",  "size": "large",  "quantity": 30 },
-				{ "id": 4, "name": "Tiny Orange Widget", "description": "A tiny, orange widget.", "color": "orange", "size": "tiny",   "quantity": 10 }
-			];
-
-			function getWidget(widgetId) {
-				return widgets.filter(function(widget) {
-			 		return widget.id === widgetId;
-			 	})[0]
-			}
-
-			function getWidgetIndex(widgetId) {
-				return widgets.indexOf(getWidget(widgetId));
-			}
-
-			var nextId = 10;
+		.factory("widgets", function($http) {
 
 			return {
 				getAll: function(options) {
-					return widgets;
+					return $http.get("/api/widgets");
 				},
 				get: function(widgetId) {
-					return getWidget(widgetId);
+					return $http.get("/api/widgetss/" + encodeURIComponent(widgetId));
 				},
 				insert: function(widget) {
 					widget.id = nextId++;
@@ -107,22 +103,96 @@
 			}
 
 		})
-		.controller("HomeCtrl", function($scope, widgets, $state) {
-			$scope.widgets = widgets.getAll();
+		// .factory("widgets", function() {
+		//
+		// 	var widgets = [
+		// 		{ "id": 1, "name": "Small Red Widget",   "description": "A small, red widget.",   "color": "red",    "size": "small",  "quantity": 2 },
+		// 		{ "id": 2, "name": "Medium Blue Widget", "description": "A medium, blue widget.", "color": "blue",   "size": "medium", "quantity": 15 },
+		// 		{ "id": 3, "name": "Large Green Widget", "description": "A large, green widget.", "color": "green",  "size": "large",  "quantity": 30 },
+		// 		{ "id": 4, "name": "Tiny Orange Widget", "description": "A tiny, orange widget.", "color": "orange", "size": "tiny",   "quantity": 10 }
+		// 	];
+		//
+		// 	function getWidget(widgetId) {
+		// 		return widgets.filter(function(widget) {
+		// 	 		return widget.id === widgetId;
+		// 	 	})[0]
+		// 	}
+		//
+		// 	function getWidgetIndex(widgetId) {
+		// 		return widgets.indexOf(getWidget(widgetId));
+		// 	}
+		//
+		// 	var nextId = 10;
+		//
+		// 	return {
+		// 		getAll: function(options) {
+		// 			return widgets;
+		// 		},
+		// 		get: function(widgetId) {
+		// 			return getWidget(widgetId);
+		// 		},
+		// 		insert: function(widget) {
+		// 			widget.id = nextId++;
+		// 			widgets.push(widget);
+		// 		},
+		// 		update: function(widget) {
+		// 			var oldWidget = widgets.splice(getWidgetIndex(widget.id), 1);
+		// 			widgets.push(widget);
+		// 		},
+		// 		delete: function(widgetId) {
+		// 			widgets.splice(getWidgetIndex(widgetId), 1);
+		// 		},
+		// 		getColors: function() {
+		// 			return [
+		// 				{ code: "blue", label: "Blue", category: "Discount" },
+		// 				{ code: "green", label: "Green", category: "Premium" },
+		// 				{ code: "red", label: "Red", category: "Discount" },
+		// 				{ code: "orange", label: "Orange", category: "Premium" }
+		// 			];
+		// 		},
+		// 		getSizes: function() {
+		// 			return [
+		// 				{ code: "tiny", label: "Tiny" },
+		// 				{ code: "small", label: "Small" },
+		// 				{ code: "medium", label: "Medium" },
+		// 				{ code: "large", label: "Large" },
+		// 			]
+		// 		}
+		// 	}
+		//
+		// })
+		.controller("HomeCtrl", function($scope, widgetsData, $state) {
+
+			// synchronous coding
+			//$scope.widgets = widgets.getAll();
+
+			// asynchronous
+			// widgets.getAll().then(function(results) {
+			// 	$scope.widgets = results.data;
+			// }).catch(function(results) {
+			// 	console.log("something went wrong...");
+			// });
+
+			$scope.widgets = widgetsData;
 
 			$scope.createWidget = function() {
 				$state.go("create");
 			};
 
-		})
-		.controller("ViewCtrl", function($scope, widgets, $stateParams, $state) {
+			$scope.$on("$stateChangeError", function() {
+				console.dir(arguments);
+				alert("Unable to load widget");
+			});
 
-			$scope.widget = widgets.get(parseInt($stateParams.widgetId, 10));
+		})
+		.controller("ViewCtrl", function($scope, widgetData, $state, $rootScope) {
+
+			$scope.widget = widgetData;
 
 			$scope.returnToList = function() {
 				//$location.path("/");
 				$state.go("home");
-			}
+			};
 
 		})
 		.controller("EditCtrl", function($scope, widgets, $stateParams, $state) {
