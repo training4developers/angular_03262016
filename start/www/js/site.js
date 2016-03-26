f3ca7f48cb59376492b0cf179ce6f781a0c0b738
Eric W. Greene <eric@ericwgreene.com>
2016-03-26 10:09:50 -0700

    more code demos

--- start/www/js/site.js
+++ start/www/js/site.js
@@ -2,35 +2,28 @@
 
 	"use strict";
 
-	// allow you to inject services
-	// controller functions
-	// filter functions
-	// directive functions
-	// factory functions
-	// service functions
-	// provider -> $get function
-	// $provide->decorator function
-	// resolve functions
+	angular.module("WidgetApp", ["ngRoute"])
+		.config(function($routeProvider, $locationProvider) {
 
-	// allow you to inject providers
-	// config functions
-	// provide functions
+			$locationProvider.html5Mode(true);
 
-	function homeCtrl($log, $scope) {
-		$scope.widgets = [
-			{ "id": 1, "name": "Small Red Widget",   "description": "A small, red widget.",   "color": "red",    "size": "small",  "quantity": 2 },
-			{ "id": 2, "name": "Medium Blue Widget", "description": "A medium, blue widget.", "color": "blue",   "size": "medium", "quantity": 15 },
-			{ "id": 3, "name": "Large Green Widget", "description": "A large, green widget.", "color": "green",  "size": "large",  "quantity": 30 },
-			{ "id": 4, "name": "Tiny Orange Widget", "description": "A tiny, orange widget.", "color": "orange", "size": "tiny",   "quantity": 10 }
-		];
-	}
+			$routeProvider
+				.when("/", {
+					controller: "HomeCtrl",
+					templateUrl: "/tpls/widgets.tpl"
+				})
+				.otherwise({
+					redirectTo: "/"
+				});
 
-	homeCtrl.$inject = ["$log","$scope"];
-
-	console.log(homeCtrl.toString());
-
-	angular.module("WidgetApp", [])
-		//.controller("HomeCtrl", ["$log","$scope",homeCtrl]);
-		.controller("HomeCtrl", homeCtrl);
+		})
+		.controller("HomeCtrl", function($scope) {
+			$scope.widgets = [
+				{ "id": 1, "name": "Small Red Widget",   "description": "A small, red widget.",   "color": "red",    "size": "small",  "quantity": 2 },
+				{ "id": 2, "name": "Medium Blue Widget", "description": "A medium, blue widget.", "color": "blue",   "size": "medium", "quantity": 15 },
+				{ "id": 3, "name": "Large Green Widget", "description": "A large, green widget.", "color": "green",  "size": "large",  "quantity": 30 },
+				{ "id": 4, "name": "Tiny Orange Widget", "description": "A tiny, orange widget.", "color": "orange", "size": "tiny",   "quantity": 10 }
+			];
+		});
 
 })(angular);
---
+++ start/www/tpls/widgets.tpl
@@ -0,0 +1,20 @@
+<table>
+	<thead>
+		<tr>
+			<th>Name</th>
+			<th>Description</th>
+			<th>Color</th>
+			<th>Size</th>
+			<th>Qty</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr ng-repeat="widget in widgets">
+			<td>{{widget.name}}</td>
+			<td>{{widget.description}}</td>
+			<td>{{widget.color}}</td>
+			<td>{{widget.size}}</td>
+			<td>{{widget.quantity}}</td>
+		</tr>
+	</tbody>
+</table>
